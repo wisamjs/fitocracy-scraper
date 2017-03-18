99aa6d1c89d33c9fc724b986776e1275aea7a8cd
wisamjs <wisamzaghal@gmail.com>
2017-03-17 20:10:59 -0400

    write to file after getting data

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var CONSTANTS = require('./constants');
 var username = CONSTANTS.username;
 var password = CONSTANTS.password;
 var Nightmare = require('nightmare');
-var nightmare = Nightmare({ show: true });
+var nightmare = Nightmare({ show: false });
 var url = 'https://www.fitocracy.com/';
 var loginModal = 'html > body.theme-minimal > div.container:nth-child(3) > div.navbar.navbar-default:nth-child(1) > div.navbar-inner:nth-child(1) > div.navbar-header.pull-right:nth-child(3) > a.button:nth-child(1)';
 var usernameField = 'form#login-modal-form > div.login-details:nth-child(2) > div.input-group:nth-child(1) > input.form-control:nth-child(2)';
@@ -10,6 +10,7 @@ var passwordField = 'form#login-modal-form > div.login-details:nth-child(2) > di
 var loginButton='form#login-modal-form > button:nth-child(3)';
 var profile = '#profile';
 var activities ='#profile-activities-tab';
+var fs = require('fs');
 
 nightmare
   .goto(url)
@@ -25,27 +26,26 @@ nightmare
   .wait(2000)
   .evaluate(function () {
     var result = [];
-
+      var contentTest;
     document.querySelectorAll('.stream_item')
     .forEach(function(element) {
-      var workouts = [];
+      var exercises = [];
 
       var date = element.querySelector('.stream-item-headline').querySelector('.gray_link').text;
-
       var workout = element.querySelectorAll('.stream-item-content .action .action_detail>li');
 
-      workout.forEach(function(element){
-        var exercise = element.querySelector('.action_prompt').textContent;
-        var lift='';
-        var lifts = element.querySelectorAll('ul li');
+      workout.forEach(function(exercise){
+        var name = exercise.querySelector('.action_prompt').textContent;
+        var content = '';
+        var contentElements = exercise.querySelectorAll('ul li');
 
-        lifts.forEach(function(element){
-          lift += element.textContent;
+        contentElements.forEach(function(element){
+          content += element.textContent;
         })
 
-      var reps = lift.match(/\d reps/g);
-      var weights = lift.match(/(\d*) lb/g);
-      var words = lift.match(/((\D )\D*)/g) || [];
+      var reps = content.match(/\d reps/g);
+      var weights = content.match(/(\d*) lb/g);
+      var words = content.match(/((\D )\D*)/g) || [];
       var comments = words[words.length - 1] || '';
       var readableComments = comments.replace(/  /g, '');
 
@@ -72,28 +72,29 @@ nightmare
         var sets = {weight: null, reps: null}
       }
 
-      var workout = {
-        exercise: exercise,
+      var exercise = {
+        name: name,
         comments: comments,
-        lift:sets
+        sets:sets
       }
-      workouts.push(workout)
+      exercises.push(exercise)
 
-    })
+    } )
 
     result.push({
       date: date,
-      workout: workouts
+      exercises: exercises
     })
 
 
   })
 
-return result;
+
+  return JSON.stringify(result);
 })
   .end()
     .then(function (result) {
-      console.log(result)
+      fs.writeFile('data.json', result, 'utf8');
     })
     .catch(function (error) {
       console.error('Error:', error);
